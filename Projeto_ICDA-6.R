31a7ee6519416d4ed0a5b753f09c31dde40bd285
Edilene <edilene.shizue@aluno.ifsp.edu.br>
2022-11-29 16:48:52 -0300

    fix: Analise Exploratória

--- Projeto_ICDA-6.R
+++ Projeto_ICDA-6.R
@@ -45,3 +45,68 @@ Mundo [1,1] #Trazendo a primeira linha e a primeira coluna
 Mundo [-1] #Exluindo a primeira coluna da análise
 
 Mundo$`Crime Index` < 50.0
+
+#Análise Exploratória=========================================================================================================
+
+names(Montreal)
+names(Mundo)
+
+#Análise Montreal
+
+summary(Montreal)
+summary(Mundo)
+
+Montreal$category <- as.factor(Montreal$category)
+Montreal$neighbourhood <- as.factor(Montreal$neighbourhood)
+Montreal$year <- as.numeric(Montreal$year)
+
+plot(Montreal$category, Montreal$year)
+
+plot(
+  Montreal$category, #cordenada X
+  Montreal$neighbourhood, #cordenada Y
+  main = "Campo Experimental Criminalidade",
+  xlab = "Crime", #legenda eixo x
+  ylab = "Vizinhança", #leganda eixo y
+  asp = 1, #aspecto gráfico (mesma escala para eixos)
+  pch =16, #tipo de caracter a ser exibido
+  cex = 3, #tamanho caracter
+  col = "red"
+)
+
+basicStats(Montreal$year) #Comportamento dos dados
+
+histPlot(as.timeSeries(Montreal$year))
+
+boxPlot(Montreal$year)
+
+ggplot(Montreal, aes(x=category, y=neighbourhood,))+
+  geom_point()
+
+#análise Mundo
+
+Mundo$`Crime Index` <- as.numeric(Mundo$`Crime Index`)
+Mundo$`Safety Index` <- as.numeric(Mundo$`Safety Index`)
+
+plot(Mundo$`Crime Index`, Mundo$`Safety Index`) #Simples plotagem
+
+plot(
+  Mundo$`Crime Index`, #cordenada X
+  Mundo$`Safety Index`, #cordenada Y
+  main = "Campo Experimental Criminalidade",
+  xlab = "Crime", #legenda eixo x
+  ylab = "Safety", #leganda eixo y
+  asp = 1, #aspecto gráfico (mesma escala para eixos)
+  pch =16, #tipo de caracter a ser exibido
+  cex = 3, #tamanho caracter
+  col = "red"
+)
+
+basicStats(Mundo$`Crime Index`) #Comportamento dos dados
+
+histPlot(as.timeSeries(Mundo$`Crime Index`))
+
+boxPlot(Mundo$`Crime Index`)
+
+ggplot(Mundo, aes(x=City, y=`Safety Index`,))+
+  geom_point()
